2eb5eeb73490696c817674e3a89bc3f731080698
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2021-11-19 01:16:36 +1100

    create draw x and o function

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-const gameBoard ={
-    // const cell = [['','',''],['','',''],['','','']]
+const cells = document.querySelectorAll(".cell")
+
+function drawCircle(){
+    cells.forEach(cell=>{
+    cell.addEventListener('click',()=>{
+        cell.textContent= '〇'
+    })
+})
 }
+
+function removePlayer(){
+    cells.forEach(cell=>
+            cell.removeEventListener('click')
+    )
+}
+
+function drawX(){
+    cells.forEach(cell=>{
+        cell.addEventListener('click',()=>{
+            cell.textContent= '✕'
+        })
+    })
+
+}
+
+
+// function swapPlayer(){
+    
+//     removePlayer();
+//     drawCircle()
+//     removePlayer();
+// }
+drawX();
